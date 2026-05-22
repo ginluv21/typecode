77b4759bb2897767f050ea97302be8390bc80cac
nepoveslo <coolreg273@gmail.com>
2026-05-22 22:18:00 +0700

    menu: accept AppConfig in menu_run and add vim-style navigation (j/k, gg, G)

--- include/menu.h
+++ include/menu.h
@@ -2,8 +2,9 @@
 #define MENU_H
 
 #include "typecode.h"
+#include "config.h"
 
 void       draw_main_menu(int selected);
-MenuOption menu_run(void);
+MenuOption menu_run(const AppConfig *cfg);
 
 #endif
